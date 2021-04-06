56a283c5c9f4a60e0b9adf9e1ca3995eb955d42b
vicso <vic.sokoliuk@gmail.com>
2021-04-06 09:11:58 +0300

    initial commit starter uploaded

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+let preprocessor = 'sass';
+const {src, dest, parallel, series, watch} = require('gulp');
+
+const browserSync = require('browser-sync').create();
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleancss = require('gulp-clean-css');
+
+
+function browsersync() {
+	browserSync.init({ 
+		server: { baseDir: 'app/' }, 
+		notify: false,
+		online: true
+	})
+}
+
+function scripts() {
+	return src([ // Берём файлы из источников
+		'node_modules/jquery/dist/jquery.min.js', // Пример подключения библиотеки
+		'app/js/app.js', // Пользовательские скрипты, использующие библиотеку, должны быть подключены в конце
+		])
+	.pipe(concat('app.min.js')) // Конкатенируем в один файл
+	.pipe(uglify()) // Сжимаем JavaScript
+	.pipe(dest('app/js/')) // Выгружаем готовый файл в папку назначения
+	.pipe(browserSync.stream()) // Триггерим Browsersync для обновления страницы
+}
+
+function styles() {
+	return src('app/' + preprocessor + '/main.' + preprocessor + '') // Выбираем источник: "app/sass/main.sass" или "app/less/main.less"
+	.pipe(eval(preprocessor)()) // Преобразуем значение переменной "preprocessor" в функцию
+	.pipe(concat('app.min.css')) // Конкатенируем в файл app.min.js
+	.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true })) // Создадим префиксы с помощью Autoprefixer
+	.pipe(cleancss( { level: { 1: { specialComments: 0 } }/* , format: 'beautify' */ } )) // Минифицируем стили
+	.pipe(dest('app/css/')) // Выгрузим результат в папку "app/css/"
+	.pipe(browserSync.stream()) // Сделаем инъекцию в браузер
+}
+
+function startwatch() {
+	watch(['app/**/*.js', '!app/**/*.min.js'], scripts);
+    watch('app/**/' + preprocessor + '/**/*', styles);
+    watch('app/**/*.html').on('change', browserSync.reload);
+}
+
+function buildcopy() {
+	return src([ // Выбираем нужные файлы
+		'app/css/**/*.min.css',
+		'app/js/**/*.min.js',
+		'app/images/dest/**/*',
+		'app/**/*.html',
+		], { base: 'app' }) // Параметр "base" сохраняет структуру проекта при копировании
+	.pipe(dest('dist')) // Выгружаем в папку с финальной сборкой
+}
+
+exports.browsersync = browsersync;
+exports.scripts = scripts;
+exports.styles = styles;
+exports.build = series(styles, scripts, buildcopy);
+exports.default = parallel(styles, scripts, browsersync, startwatch);
